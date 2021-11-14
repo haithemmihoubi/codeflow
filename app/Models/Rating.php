9b889335b3b9e8bcff822b73fccebd344f9048b2
haithemmihoubi <haithemmihoubi1234@gmail.com>
2021-11-14 15:28:36 +0100

    connecting all models to each other

--- app/Models/Rating.php
+++ app/Models/Rating.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use phpDocumentor\Reflection\Types\This;
 
 class Rating extends Model
 {
     use HasFactory;
+
+    /* Match between the rating and the post
+     *
+     * */
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    } /* Match between the rating and the post
+     *
+     * */
+    public function user()
+    {
+        return $this->belongsTo(Post::class);
+    }
 }

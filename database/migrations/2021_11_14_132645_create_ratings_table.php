4b471a398ad9cc09f954ac0453e50dc3b06b0925
haithemmihoubi <haithemmihoubi1234@gmail.com>
2021-11-14 14:52:37 +0100

    generating Rating.php table + RatingController..php Rating.php

---
+++ database/migrations/2021_11_14_132645_create_ratings_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRatingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ratings', function (Blueprint $table) {
+            $table->id();
+            //The rating value
+            $table->integer("value") ;
+            //getting the post id
+            $table->foreignId('post_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            //getting the post id for current logged in user who comments a post
+            // delete a comment that means delete everything on cascade
+            $table->foreignId('user_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ratings');
+    }
+}

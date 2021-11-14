5a50ad7148647a5c72e47677cdfed074ae11c666
haithemmihoubi <haithemmihoubi1234@gmail.com>
2021-11-14 14:11:53 +0100

    generating Comments table + CommentController..php Comment.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('photo');
+            $table->enum('role',['admin','user'])->default("user");
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();

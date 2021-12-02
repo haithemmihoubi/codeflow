f0f167e6dfab6d59fc9a81799cc97d30801c9227
safwensoker <safwensoker1@gmail.com>
2021-12-02 15:19:41 +0100

    Configuring the app : adding name and alias

--- config/app.php
+++ config/app.php
@@ -12,8 +12,9 @@ return [
     | any other location as required by the application or its packages.
     |
     */
-
-    'name' => env('APP_NAME', 'Laravel'),
+    'app_name' => 'Codeflow',
+    'subtitle' => 'You can learn',
+    'name' => env('APP_NAME', 'Codeflow'),
 
     /*
     |--------------------------------------------------------------------------

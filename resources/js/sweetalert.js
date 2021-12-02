76a22cc44e03b382b4e85ebedb1ab590636d1f5f
safwensoker <safwensoker1@gmail.com>
2021-12-02 15:25:16 +0100

    Adding css and js by compiling assets

---
+++ resources/js/sweetalert.js
@@ -0,0 +1,15 @@
+
+window.deleteConfirm = function(formId)
+{
+    Swal.fire({
+        icon: 'warning',
+        text: 'Do you want to delete this?',
+        showCancelButton: true,
+        confirmButtonText: 'Delete',
+        confirmButtonColor: '#e3342f',
+    }).then((result) => {
+        if (result.isConfirmed) {
+            document.getElementById(formId).submit();
+        }
+    });
+}

91bd0d1e0abf30e4d385d59d40c8551c8fe7ec94
safwensoker <safwensoker1@gmail.com>
2021-12-05 05:07:45 +0100

    configuring the admin dashboard

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
  <head>
-   @include('shared.head')
+   @include('layouts.head')
  </head>
  <body>
-@include('shared.nav')
-@include('shared.sidenav')
-@include('shared.footer')
+@include('layouts.nav')
+<style>
+  div.card, div.jumbotron{
+    box-shadow: #C8F7FF 0px 10px 10px, #0099C7 0px -12px 30px, #C8F7FF 0px 4px 6px, #C8F7FF 0px 12px 13px, #C8F7FF 0px -3px 5px;
+  }
+</style>
+@include('layouts.sidenav')
+@include('layouts.footer')
  </body>
 </html>

b033df0804186192a6c88799d1e223b1dc94dadd
safwensoker <safwensoker1@gmail.com>
2021-12-05 05:01:55 +0100

    adding flash meesage +  update and delete operations

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -1,5 +1,13 @@
 @extends('admin.index')
 @section('main')
+@if ($message = Session::get('success'))
+    <div class="alert alert-success alert-dismissible fade show mt-5" role="alert">
+        <strong>{{ $message }}</strong> 
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+@endif
 <div class="card mb-4">
     <div class="card-header">
         <i class="fas fa-table me-1"></i>
@@ -21,23 +29,12 @@
                 <td>{{$category->name}}</td>
                 <td>{{$category->description}}</td>
                 <td>
-                    <a href="{{ route('admin.categories.show', ['category' => $category->id]);}}" class="btn btn-primary"><i class="fas fa-eye"></i></a>
-                    <form action="{{ url('categories/'.$category->id) }}" method="POST">
-                        {{ csrf_field() }}
-                        {{ method_field('PUT') }}
-            
-                        <button type="submit" class="btn btn-secondary">
-                            <i class="fas fa-edit"></i>
-                        </button>
-                    </form>
-                    <form action="{{ url('categories/'.$category->id) }}" method="POST">
-                        {{ csrf_field() }}
-                        {{ method_field('DELETE') }}
-            
-                        <button type="submit" class="btn btn-danger">
-                            <i class="fa fa-trash"></i>
-                        </button>
-                    </form>
+                    <a href="{{ route('admin.categories.show', ['category' => $category->id]);}}" class="btn btn-outline-secondary"><i class="fas fa-eye"></i></a>
+                    <a href="{{ route('admin.categories.edit', ['category' => $category->id])}}" title="Edit category {{ $category->name }}" class="btn btn-outline-warning"><i class="fas fa-edit"></i></a>
+                    <a href="#" 
+                    onClick="event.preventDefault();
+                    document.querySelector('#delete-category-form').submit()" class="btn btn-outline-danger" title="Delete category {{ $category->name }}"><i class="far fa-times-circle"></i> Delete this category</a>
+                    <form action="{{route('admin.categories.destroy', ['category' => $category->id])}}" method="post" id="delete-category-form">@csrf @method('DELETE') </form>
                 </td>
             </tr>    
             @endforeach

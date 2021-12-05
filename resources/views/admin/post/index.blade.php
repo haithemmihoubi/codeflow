3219caeffa74a1660eb3b195734458d8c779d7cb
safwensoker <safwensoker1@gmail.com>
2021-12-05 05:04:04 +0100

    adding flash meesage +  approve and delete operations

--- resources/views/admin/post/index.blade.php
+++ resources/views/admin/post/index.blade.php
@@ -1,51 +1,56 @@
 @extends('admin.index')
 @section('main')
-
-<div class="card mb-4">
-    <div class="card-header">
-        <i class="fas fa-table me-1"></i>
-        Posts to validate
-        <a href="{{ route('admin.posts.create') }}" class="btn btn-outline-primary float-right"><i class="fas fa-user-plus"></i> Add Post</a>
+@if ($message = Session::get('success'))
+    <div class="alert alert-success alert-dismissible fade show mt-5" role="alert">
+        <strong>{{ $message }}</strong> 
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        </button>
     </div>
-    <div class="card-body">
-        <table id="datatablesSimple">
-            <thead>
+@endif
+<div class="container">
+    <div class="card mb-4 mt-5">
+        <div class="card-header">
+            <i class="fas fa-table me-1"></i>
+            Posts to validate
+            <a href="{{ route('admin.posts.create') }}" class="btn btn-outline-primary float-right"><i class="fas fa-user-plus"></i> Add Post</a>
+        </div>
+        <div class="card-body">
+            <table id="datatablesSimple">
+                <thead>
+                    <tr>
+                        <th>Title</th>                    
+                        <th>Category</th>
+                        <th>Approved</th>
+                        <th>Operations</th>    
+                    </tr>
+                </thead>
+                <tbody>
+                @foreach ($posts as $post)
                 <tr>
-                    <th>Title</th>                    
-                    <th>Category</th>
-                    <th>Operations</th>    
-                </tr>
-            </thead>
-            <tbody>
-            @foreach ($posts as $post)
-            <tr>
-                <td>{{$post->title}}</td>
-                <td>{{$post->category->name}}</td>
-                <td>
-                    <a href="{{ route('posts.show', ['post' => $post->id]);}}" class="btn btn-primary"><i class="fas fa-eye"></i></a>
-                    <form action="{{ url('posts/'.$post->id) }}" method="POST">
-                        {{ csrf_field() }}
-                        {{ method_field('PUT') }}
-            
-                        <button type="submit" class="btn btn-secondary">
-                            <i class="fas fa-edit"></i>
-                        </button>
-                    </form>
-                    <form action="{{ url('posts/'.$post->id) }}" method="POST">
-                        {{ csrf_field() }}
-                        {{ method_field('DELETE') }}
-            
-                        <button type="submit" class="btn btn-danger">
-                            <i class="fa fa-trash"></i>
-                        </button>
-                    </form>
-                </td>
-            </tr>    
-            @endforeach
-            </tbody>
-        </table>
-    </div>
+                    <td>{{$post->title}}</td>
+                    <td>{{$post->category->name}}</td>
+                    <td>
+                        @if ($post->validated)
+                            Yes
+                        @else
+                            No
+                        @endif
+                    </td>
+                    <td>
+                        <a href="{{ route('admin.posts.show', ['post' => $post->id]);}}" title="Show post {{ $post->title }}" class="btn btn-outline-secondary"><i class="fas fa-eye"></i></a>
+                        <a href="#" 
+                        onClick="event.preventDefault();
+                        document.querySelector('#delete-post-form').submit()" class="btn btn-outline-danger" title="Delete post {{ $post->title }}"><i class="far fa-times-circle"></i></a>
+                        <a href="{{route('admin.posts.validatePost', ['post' => $post->id])}}" class="btn btn-outline-success"><i class="far fa-check-circle"></i> Approve this post</a>
+                        <form action="{{route('admin.posts.destroy', ['post' => $post->id])}}" method="post" id="delete-post-form">@csrf @method('DELETE') </form>
+                    </td>
+                </tr>    
+                @endforeach
+                </tbody>
+            </table>
+        </div>
 </div>    
-
+</div>
 
 @endsection
